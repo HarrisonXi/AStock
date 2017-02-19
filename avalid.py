a62716ebb298fdfcf5ee7985161cd85bc03bc17c
HarrisonXi <gpra8764@gmail.com>
2017-02-19 15:21:14 +0800

    生成新的有效股票列表（剔除新股）

--- avalid.py
+++ avalid.py
@@ -9,8 +9,8 @@ threadLock = threading.Lock()
 validPattern = re.compile(r'var hq_str_(s[hz]\d{6})="[^,"]+,[^,"]+,[^,"]+,[^,"]+,[^"]+";')
 # 确认存在的3个特殊股票代码
 validStockList = ["sz001696","sz001896","sz001979"]
-# 2016年7月才上市的股票代码
-newStockList = ["sz002803","sz002805","sz002806","sz002807","sz002808","sh600919","sh600936","sh600977","sh601595","sh601811","sh601966","sh601997","sh603007","sh603016","sh603069","sh603159","sh603322","sh603515","sh603569","sh603663","sh603843","sh603986"]
+# 2017年1月1日及之后才上市的股票代码
+newStockList = ["sh603955","sh603178","sh603138","sz002851","sh603717","sh603578","sz002850","sh603388","sh603768","sh603238","sh603345","sh600939","sh603817","sh603603","sz002849","sh601212","sz002848","sh603615","sh603330","sh603208","sh603839","sh603626","sh603040","sh603637","sh603177","sz002847","sz002846","sh603360","sh603677","sh603881","sh603089","sh603358","sz002845","sh603966","sh603429","sz002839","sh601881","sh603638","sz002843","sh603037","sz002841","sh603337","sh601858","sh603165","sz002842","sh603668","sh603038","sh603690","sh603039","sz002824","sh603639","sh603579","sh603628","sh603266","sh603689","sh603877","sz002840","sh603228","sz002838","sh603035","sh603032","sh603186","sh603444"]
 
 def filterStockList(stockList):
 	url = 'http://hq.sinajs.cn/list=' + ','.join(stockList)
@@ -42,7 +42,7 @@ threadList = []
 for index in xrange(1, 1000, 100):
 	thread = threading.Thread(target = threadFunction, args = ('sz', index, index + 100))
 	threadList.append(thread)
-for index in xrange(2001, 2809, 100):
+for index in xrange(2001, 2850, 100):
 	thread = threading.Thread(target = threadFunction, args = ('sz', index, index + 100))
 	threadList.append(thread)
 for index in xrange(600000, 602000, 100):
@@ -59,6 +59,8 @@ for stockCode in newStockList:
 	if stockCode in validStockList:
 		validStockList.remove(stockCode)
 validStockList.sort()
-print("['" + "','".join(validStockList) + "']")
+validStockFile = open('stock.list', 'w')
+validStockFile.write('\n'.join(validStockList))
+validStockFile.close()
 print('总数量: %d' % (len(validStockList)))
 print('总用时: %.2f秒' % (time.time() - startTime))
